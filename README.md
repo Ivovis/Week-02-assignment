5ab607854d044c6423d835e1808c72aa1888ffac
Christopher Hunn <christopher.hunn@gmail.com>
2025-05-09 15:15:03 +0100

    feat: display background image from index number

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ An accessible image gallery following a mobile-first approach and responsive des
 - dug out some clip art files I created as bitmapped images using an AI generator last year, the original images were ( back then not now )then converted with a fair bit of curation using python scripts into svg format that I am using today, svg gives be two advantages 1. they are very small in size and 2. they scale to any size so I can avoid all the work of creating different resolutions.
 
   I did a quick ls>namelist.txt in the media directory to make creating the array I need later faster.
+
+Created two wire frames one for mobile
+
+<img src="./media/mobile-wireframe.png">
+
+the other for desktop
+
+<img src="./media/Desktop-wireframe.png">
+
+- so far so good no hurdles other than using the http link to clone the repo - fixed.
--- app.js
+++ app.js
@@ -1,1 +1,68 @@
-console.log("check");
+const doc = Document;
+const mediaArray = [
+  {
+    imageID: 0,
+    imageSrc: "./media/2024-12-02_16-40-55_2715-Frogs-0015.svg",
+    imageAlt: "Majestic frog facing slightly left.",
+  },
+  {
+    imageID: 1,
+    imageSrc: "./media/2024-12-02_17-31-24_3135-Squirrels-0003.svg",
+    imageAlt: "Squirrel sitting on its haunches, pretending to hold a nut.",
+  },
+  {
+    imageID: 2,
+    imageSrc: "./media/2024-12-04_11-45-44_4441-Hedgehog-0002.svg",
+    imageAlt: "A cute hedgehog standing on four short legs, facing left",
+  },
+];
+
+function updateBackground(idIndex) {
+  // ask me if I've ever done this before!
+  if (idIndex >= mediaArray.length) {
+    console.log("updateBackground called with out of range index - ignored");
+    return;
+  }
+
+  // get id of any existing element with class of "theBackground"
+  let existingID = document.getElementsByClassName("theBackground");
+
+  // check if we have one, or more - what happens if we have more? do we get an array or null or just the first found - so many questions so little time.
+  // check if id is the same as current displayed image - if so just exit
+
+  // make img element
+  let newImage = document.createElement("img");
+
+  // set image source
+  newImage.setAttribute("src", mediaArray[idIndex].imageSrc);
+  // set alt text
+  newImage.setAttribute("alt", mediaArray[idIndex].imageAlt);
+  // set class
+  newImage.setAttribute("class", "theBackground");
+  // set an id to the index
+  newImage.setAttribute("id", mediaArray[idIndex].imageID);
+
+  // add it to the container
+  document.getElementById("backgroundID").appendChild(newImage);
+
+  // remove old image if it exists (or use for transition later)
+}
+
+updateBackground(2);
+
+// list of media still to add to the media array
+//
+// 2024-12-02_16-59-55_3802-Frogs-0049.svg
+//
+// 2024-12-02_17-38-17_2518-Squirrels-0018.svg
+// 2024-12-04_11-45-44_4441-Hedgehog-0002.svg
+// 2024-12-04_12-36-06_6434-Rabbit-0004.svg
+// 2024-12-04_12-38-54_8810-Rabbit-0011.svg
+// 2024-12-04_13-36-31_7882-Elephant-0001.svg
+// 2024-12-04_14-17-49_8608-Elephant-0085.svg
+// 2024-12-04_15-33-20_1483-Duck-0010.svg
+// 2024-12-04_16-39-36_8198-BearLogo-0047.svg
+// 2024-12-04_17-08-36_3238-BearLogo-0070.svg
+// 2024-12-05_13-03-06_9318-Cat-0014.svg
+// 2024-12-05_13-49-49_8735-Chicken-0008.svg
+// 2024-12-05_13-50-37_2575-Chicken-0009.svg
